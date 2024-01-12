d1f9f6217e7faf25683d1b9561995a1820738209
shubham <shubhamkakad10x@gmail.com>
2024-01-12 12:07:57 +0530

    DB connection

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import dotenv from 'dotenv'    // conf required 
+import connectDB from './db/index.js'
+dotenv.config({path:'./env'})
+
+
+// database function call
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// database connection in index file directly  option-1
+import mongoose from "mongoose";
+import express from "express";
+import {DB_NAME} from './constants.js'
+const app = express()
+;(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (error) =>{
+            console.log('error', error);
+            throw error    
+        })
+        
+        app.listen(process.env.PORT, () => {
+            console.log(` app is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.log('mongobd connection failed', error);
+        throw error
+    }
+})()
+*/
